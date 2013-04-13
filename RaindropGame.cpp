a2fd378abb3d9a565e4def094cb55a1a8d6fb937
Roselle Milvich <rose@me.com>
2013-04-13 14:56:00 -0600

    going to find the stutter

--- RaindropGame.cpp
+++ RaindropGame.cpp
@@ -15,7 +15,7 @@ RaindropGame::RaindropGame(string fname, int cups, int drops, int speed, int lat
     
     //set background
     background = new Sprite("background.txt");
-    sprites.insert(sprites.begin(), background);
+    backgrounds.insert(backgrounds.begin(), background);
     
     //set pane
     pane = new Pane();
@@ -28,7 +28,8 @@ RaindropGame::RaindropGame(string fname, int cups, int drops, int speed, int lat
 
 void RaindropGame::run(){
     SDL_Event event;
-    Uint32 last = SDL_GetTicks();
+    last = SDL_GetTicks();
+    lastDrop = last;
     SDL_Delay(100);
     
     //add cups
@@ -42,25 +43,11 @@ void RaindropGame::run(){
 
     //run loop
     while (!done) {
-        Uint32 elapsed = SDL_GetTicks();
-        
-        //add drops if needed
-        while (drops.size() < numDrops && elapsed > last + minLatency) {
-            int x = (rand()%20)*0.05*SCREENWIDTH;
-            Drop *d = new Drop("a.txt", PLAIN, x, gameSpeed);
-            drops.insert(drops.end(), d);
-            last = SDL_GetTicks();
-        }
-        
-        //erase drops that are going offscreen
-        for (int i = (int)drops.size()-1; i >= 0; i--) {
-            if (drops[i]->isAlive() == 0) {
-                Drop *d = drops[i];
-                drops.erase(drops.begin()+i);
-                delete d;
-            }
-        }//for all, for each, algorithms part
-        
+        elapsed = SDL_GetTicks();
+        if(1000/100 > (elapsed-last))
+			SDL_Delay(1000/100 - (elapsed-last)); //RESTRICT PLAYBACK TO 30 FRAMES A SECOND
+        last = elapsed;
+
         //events loop
         while (SDL_PollEvent(&event)){//checks events one at a time
 			switch( event.type ){
@@ -103,24 +90,29 @@ void RaindropGame::run(){
         }//ends event detection while loop
         
         
-        //check for collisions and update if needed
+        //check for water in cup and update if needed
         Cup::checkCollisions(cups,drops);
         
-        //update background
-        for (int i = 0; i < sprites.size(); i++) {
-            sprites[i]->update(elapsed);
-            sprites[i]->draw(screen, elapsed);
+        //update background, drops, cups
+        SDL_Thread *upThread;
+        upThread = SDL_CreateThread(updateThread, this);
+        if(upThread == NULL){
+            cout<<"Error starting upThread..."<<endl;
         }
         
-        //update drops
+        
+        //draw background
+        for (int i = 0; i < backgrounds.size(); i++) {
+            backgrounds[i]->draw(screen, elapsed);
+        }
+        
+        //draw drops
         for (int i = 0; i < drops.size(); i++) {
-            drops[i]->update(elapsed);
             drops[i]->draw(screen, elapsed);
         }
         
-        //update cups
+        //draw cups
         for (int i = 0; i < cups.size(); i++) {
-            cups[i]->update(elapsed);
             cups[i]->draw(screen, elapsed);
         }
         
@@ -158,7 +150,45 @@ bool RaindropGame::checkClickCup(int x, int y){
     return 0;
 }
 
+int RaindropGame::updateThread(void *ptr){
+    RaindropGame* game = (RaindropGame*)ptr;
+    //add drops if needed
+    while (game->drops.size() < game->numDrops && game->elapsed > game->lastDrop + game->minLatency) {
+        int x = (rand()%20)*0.05*SCREENWIDTH;
+        Drop *d = new Drop("a.txt", PLAIN, x, game->gameSpeed);
+        game->drops.insert(game->drops.end(), d);
+        game->lastDrop = SDL_GetTicks();
+    }
+    
+    //erase drops that are going offscreen
+    for (int i = (int)game->drops.size()-1; i >= 0; i--) {
+        if (game->drops[i]->isAlive() == 0) {
+            Drop *d = game->drops[i];
+            game->drops.erase(game->drops.begin()+i);
+            delete d;
+        }
+    }//for all, for each, algorithms part
+    
+    //update background
+    for (int i = 0; i < game->backgrounds.size(); i++) {
+        game->backgrounds[i]->update(game->elapsed);
+    }
+    
+    //update drops
+    for (int i = 0; i < game->drops.size(); i++) {
+        game->drops[i]->update(game->elapsed);
+    }
+    
+    //update cups
+    for (int i = 0; i < game->cups.size(); i++) {
+        game->cups[i]->update(game->elapsed);
+    }
+    return 0;
+}
+
 RaindropGame::~RaindropGame(){
+//    if (upThread) SDL_KillThread(evtThread);
+
     for (int i = 0; i<drops.size(); i++){
         delete drops[i];
     }
--- RaindropGame.h
+++ RaindropGame.h
@@ -3,10 +3,13 @@
 
 #ifdef _WIN32
 #include "SDL/SDL.h"
+#include "SDL/SDL_thread.h"
+
 #elif __APPLE__
 #include <SDL/SDL.h>
 #include <SDL/SDL_main.h>
 #include <SDL_image/SDL_image.h>
+#include <SDL/SDL_thread.h>
 
 #elif __unix__
 #endif
@@ -25,18 +28,26 @@ class RaindropGame: public Game{
     int numDrops;
     int gameSpeed;
     int minLatency;
+    vector<Sprite*> backgrounds;
     vector<Drop*> drops;
     vector<Cup*> cups;
     float timestampMouseDown;
     bool isDragging;
+    Uint32 elapsed;
+    Uint32 last;
+    Uint32 lastDrop;
 //    Sprite *objDragged;
     Sprite *background;
     Pane *pane;
     Cup *objDragged;
     SoundPlayer *soundplayer;
-    
+
+
     bool checkClickCup(int x, int y);
     void setDraggedObject(int x, int y);
+    
+    static int updateThread(void *ptr);
+
 public:
     RaindropGame(string fname, int cups = 3, int drops = 10, int speed = 50, int latency = 800);
     void run();
