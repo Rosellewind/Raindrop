245d6a5675e99fe597d242ac86fac42ef4e4fa20
Jared <JaredTS486@gmail.com>
2013-04-13 20:16:16 -0600

    Delete Unused files generated by Eclipse, still trying to keep these from wanting to be commited.

--- EclipseBuild/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lmingw32 -lSDLmain -lSDL -lSDL_image -lSDL_mixer -lSDL_ttf
-
